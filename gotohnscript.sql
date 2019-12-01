6e6f8aa7fb36c01724f67aa613c7db9ae51c4b70
MendezOscar <oscarito_16m@live.com.mx>
2019-12-01 13:01:24 -0600

    Add script db

--- gotohnscript.sql
+++ gotohnscript.sql
@@ -1,94 +1,89 @@
-create table lugar(
-	lugarid int generated always as identity not null primary key,
-	nombre varchar
-);
 
-create table jornada (
-	jornadaid int generated always as identity not null primary key,
-	nombre varchar
-);
-
-create table categoria (
-	categoriaid int generated always as identity not null primary key,
-	nombre varchar not null
-);
-
-create table usuario(
-	usuarioid int generated always as identity not null primary key,
-	nombre varchar not null,
-	clave varchar not null,
-	tipo int not null
-);
-
-create table preferencia (
-	prefeenciaid int generated always as identity not null primary key,
-	usuarioid int not null,
-	lugarid int not null,
-	categoriaid int not null,
-	jornadaid int not null
-);
-
-alter table preferencia add constraint fk_usuarioid
-foreign key (usuarioid) references usuario(usuarioid);
-
-alter table preferencia add constraint fk_lugarid
-foreign key (lugarid) references lugar(lugarid);
-
-alter table preferencia add constraint fk_categoriaid
-foreign key (categoriaid) references categoria(categoriaid);
-
-alter table preferencia add constraint fk_jornadaid
-foreign key (jornadaid) references jornada(jornadaid);
-
-create table actividades (
-	actividadid int generated always as identity not null primary key,
-	nombre varchar not null,
-	description varchar not null,
-	horario varchar,
-	lugarid int not null,
-	categoriaid int not null,
-	jornadaid int not null
+CREATE TABLE public.categoria
+(
+    categoriaid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nombre character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT categoria_pkey PRIMARY KEY (categoriaid)
 )
 
-alter table actividades add constraint fk_lugarid
-foreign key (lugarid) references lugar(lugarid);
-
-alter table actividades add constraint fk_categoriaid
-foreign key (categoriaid) references categoria(categoriaid);
-
-alter table actividades add constraint fk_jornadaid
-foreign key (jornadaid) references jornada(jornadaid);
-
-create table seleccion (
-	seleccionid int generated always as identity not null primary key,
-	actividadid int not null,
-	prefeenciaid int not null,
-	prioridad int not null
+CREATE TABLE public.jornada
+(
+    jornadaid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nombre character varying COLLATE pg_catalog."default",
+    CONSTRAINT jornada_pkey PRIMARY KEY (jornadaid)
 )
 
-alter table seleccion add constraint fk_actividadid
-foreign key (actividadid) references actividades(actividadid);
-
-alter table seleccion add constraint fk_prefeenciaid
-foreign key (prefeenciaid) references preferencia(prefeenciaid);
-
-create table itinerario_encabezado (
-	itinierarioid int generated always as identity not null primary key,
-	prefeenciaid int not null
+CREATE TABLE public.lugar
+(
+    lugarid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nombre character varying COLLATE pg_catalog."default",
+    lugar character varying COLLATE pg_catalog."default",
+    foto character varying COLLATE pg_catalog."default",
+    horaentrada date,
+    horacierre date,
+    CONSTRAINT lugar_pkey PRIMARY KEY (lugarid)
 )
 
-alter table itinerario_encabezado add constraint fk_prefeenciaid
-foreign key (prefeenciaid) references preferencia(prefeenciaid);
-
-create table itinerario_detalle (
-	itinerario_detalleid int generated always as identity not null primary key,
-	itinierarioid int not null,
-	actividadid int not null
+CREATE TABLE public.usuario
+(
+    usuarioid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nombre character varying COLLATE pg_catalog."default" NOT NULL,
+    clave character varying COLLATE pg_catalog."default" NOT NULL,
+    tipo integer NOT NULL,
+    CONSTRAINT usuario_pkey PRIMARY KEY (usuarioid)
 )
 
-alter table itinerario_detalle add constraint fk_itinierarioid
-foreign key (itinierarioid) references itinerario_encabezado(itinierarioid);
 
-alter table itinerario_detalle add constraint fk_actividadid
-foreign key (actividadid) references actividades(actividadid);
+CREATE TABLE public.itinerario_encabezado
+(
+    itinierarioid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    fechainicio date,
+    fechafinal integer,
+    nombre character varying COLLATE pg_catalog."default",
+    lugarid integer,
+    CONSTRAINT itinerario_encabezado_pkey PRIMARY KEY (itinierarioid)
+)
+
+
+CREATE TABLE public.itinerario_detalle
+(
+    itinerario_detalleid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    itinierarioid integer NOT NULL,
+    actividadid integer NOT NULL,
+    prioridad integer,
+    CONSTRAINT itinerario_detalle_pkey PRIMARY KEY (itinerario_detalleid),
+    CONSTRAINT fk_actividadid FOREIGN KEY (actividadid)
+        REFERENCES public.actividades (actividadid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_itinierarioid FOREIGN KEY (itinierarioid)
+        REFERENCES public.itinerario_encabezado (itinierarioid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+CREATE TABLE public.actividades
+(
+    actividadid integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nombre character varying COLLATE pg_catalog."default" NOT NULL,
+    description character varying COLLATE pg_catalog."default" NOT NULL,
+    horario character varying COLLATE pg_catalog."default",
+    lugarid integer NOT NULL,
+    categoriaid integer NOT NULL,
+    jornadaid integer NOT NULL,
+    duracion date,
+    CONSTRAINT actividades_pkey PRIMARY KEY (actividadid),
+    CONSTRAINT fk_categoriaid FOREIGN KEY (categoriaid)
+        REFERENCES public.categoria (categoriaid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_jornadaid FOREIGN KEY (jornadaid)
+        REFERENCES public.jornada (jornadaid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_lugarid FOREIGN KEY (lugarid)
+        REFERENCES public.lugar (lugarid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
